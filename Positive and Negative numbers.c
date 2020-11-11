26749b079f6fcbc6794f2430aafbf5a3f4154437
ErollDelfin <73935401+ErollDelfin@users.noreply.github.com>
2020-11-11 14:55:29 +0800

    Add files via upload

---
+++ Positive and Negative numbers.c
@@ -0,0 +1,12 @@
+int main()
+{
+    int a;
+    printf("Enter a number:");
+    scanf("%d",&a);
+
+    if(a >= 0)
+        printf("%d is  a positive number.",a);
+    else
+        printf("%d is a negative number.",a);
+    return 0;
+}
